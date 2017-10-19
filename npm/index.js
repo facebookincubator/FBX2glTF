946f12361ca5a2d9342597b1eaf522fbacbac2e1
Michael Bayne <mdb@samskivert.com>
2017-10-19 10:14:13 -0700

    Numerous improvements to Node API.
    - Removed the shell scripts. We now invoke everything straight from Node.
    
    - Allow passing command line arguments to the tool via the Node API.
    
    - Require .glb or .gltf extension because the tool is automatically going to
      add that extension anyway, so we strip it off and add it back to shield the
      user from this weirdness. The tool may eventually stop adding an extension
      (and perhaps just validate it) and we can simplify our code.
    
    - Automatically add --binary option if the requested target file is a .glb.
    
    This also renames the bin/Windows directory to bin/Windows_NT, which is
    unfortunate but that matches os.type(). This ultimately comes from uname and
    that's what Windows chose to return. Let's just live with this historical
    accident rather than try to paper over it.

--- npm/index.js
+++ npm/index.js
@@ -1,6 +1,8 @@
 const childProcess = require('child_process');
+const fs = require('fs');
 const os = require('os');
 const path = require('path');
+const rimraf = require('rimraf');
 
 const binaries = {
   'darwin': `bin/darwin/Fbx2Gtlf`,
@@ -8,36 +10,70 @@ const binaries = {
   'win32': `bin\windows\Fbx2Gtlf.exe`,
 };
 
-function fbx2glb(srcFile, destFile, cwd) {
+/**
+ * Converts an FBX to a GTLF or GLB file.
+ * @param string srcFile path to the source file.
+ * @param string destFile path to the destination file.
+ * This must end in `.glb` or `.gltf` (case matters).
+ * @param string[] [opts] options to pass to the converter tool.
+ * @return Promise<string> a promise that yields the full path to the converted
+ * file, an error on conversion failure.
+ */
+function convert(srcFile, destFile, opts) {
   return new Promise((resolve, reject) => {
-    let script = os.type() === 'Windows_NT' ? 'fbx2glb.bat' : 'fbx2glb.sh';
-    let child;
     try {
-      let opts = {};
-      cwd && (opts.cwd = cwd);
-      child = childProcess.spawn(
-        path.join(__dirname, 'bin', script),
-        [ srcFile, destFile ],
-        opts
-      );
+      let tool = path.join(__dirname, 'bin', os.type(), 'FBX2glTF');
+      if (!fs.existsSync(tool)) {
+        throw new Error(`Unsupported OS: ${os.type()}`);
+      }
+
+      let destExt;
+      if (destFile.endsWith('.glb')) {
+        destExt = '.glb';
+        opts.includes('--binary') || opts.push('--binary');
+      } else if (destFile.endsWith('.gltf')) {
+        destExt = '.gltf';
+      } else {
+        throw new Error(`Unsupported file extension: ${destFile}`);
+      }
+
+      let srcPath = fs.realpathSync(srcFile);
+      let destDir = fs.realpathSync(path.dirname(destFile));
+      let destPath = path.join(destDir, path.basename(destFile, destExt));
+
+      let args = opts.slice(0);
+      args.push('--input', srcPath, '--output', destPath);
+      let child = childProcess.spawn(tool, args);
+
+      let output = '';
+      child.stdout.on('data', (data) => output += data);
+      child.stderr.on('data', (data) => output += data);
+      child.on('error', reject);
+      child.on('close', code => {
+        // the FBX SDK may create an .fbm dir during conversion; delete!
+        let fbmCruft = srcPath.replace(/.fbx$/i, '.fbm');
+        // don't stick a fork in things if this fails, just log a warning
+        const onError = error =>
+          error && console.warn(`Failed to delete ${fbmCruft}: ${error}`);
+        try {
+          fs.existsSync(fbmCruft) && rimraf(fbmCruft, {}, onError);
+        } catch (error) {
+          onError(error);
+        }
+
+        // non-zero exit code is failure
+        if (code != 0) {
+          reject(new Error(`Script ${script} output:\n` +
+                           (output.length ? output : "<none>")));
+        } else {
+          resolve(destPath + destExt);
+        }
+      });
+
     } catch (error) {
       reject(error);
-      return;
     }
-    let output = '';
-    child.stdout.on('data', (data) => output += data);
-    child.stderr.on('data', (data) => output += data);
-    child.on('error', reject);
-    child.on('close', code => {
-      // non-zero exit code is failure
-      if (code != 0) {
-        reject(new Error(`Script ${script} output:\n` +
-                         (output.length ? output : "<none>")));
-      } else {
-        resolve(destFile);
-      }
-    });
   });
 }
 
-module.exports = fbx2glb;
+module.exports = convert;
