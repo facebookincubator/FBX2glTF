571fd22d568770dc0c4ba9d39b3af1b806262cb4
Par Winzell <zell@fb.com>
2018-02-25 17:25:18 -0800

    Use STB for image info; inspect pixels for alpha.
    Previous to this, a PNG that was on RGBA format would cause its
    corresponding texture to be flagged as transparent. This is very
    silly. We now iterate over the bytes, and if any is not 255, THEN
    there's alpha.

--- src/Raw2Gltf.cpp
+++ src/Raw2Gltf.cpp
@@ -12,9 +12,7 @@
 #include <iostream>
 #include <fstream>
 
-#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
 
 #include "FBX2glTF.h"
