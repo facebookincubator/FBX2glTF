366e904b706b3568e1df92f98d69085c2cda7e4f
Par Winzell <zell@fb.com>
2019-04-22 08:45:05 -0700

    FB Copyright compliance.

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,15 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+#
+
 import os
 
 from conans import ConanFile, CMake
 
+
 class FBX2glTFConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = (("boost_filesystem/1.69.0@bincrafters/stable"),
-               ("fmt/5.3.0@bincrafters/stable"))
+                ("fmt/5.3.0@bincrafters/stable"))
     generators = "cmake_find_package", "cmake_paths"
 
     def build(self):
