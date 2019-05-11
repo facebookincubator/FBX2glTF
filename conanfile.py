0ba34ee7f50d35300a4f93439f3c464793136428
Pär Winzell <par.winzell@alyx.com>
2019-05-11 16:38:29 -0700

    settings.libcxx -> settings.compiler.libcxx

--- conanfile.py
+++ conanfile.py
@@ -8,16 +8,23 @@ from conans import ConanFile, CMake
 
 class FBX2glTFConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = (("boost_filesystem/1.69.0@bincrafters/stable"),
-                ("fmt/5.3.0@bincrafters/stable"))
+    requires = (
+        ("boost_filesystem/1.69.0@bincrafters/stable"),
+        ("fmt/5.3.0@bincrafters/stable"),
+    )
     generators = "cmake_find_package", "cmake_paths"
 
     def configure(self):
-        if self.settings.compiler == "gcc" and self.settings.libcxx == "libstdc++":
-            raise Exception("Rerun 'conan install' with argument: '-s compiler.libcxx=libstdc++11'")
+        if (
+            self.settings.compiler == "gcc"
+            and self.settings.compiler.libcxx == "libstdc++"
+        ):
+            raise Exception(
+                "Rerun 'conan install' with argument: '-s compiler.libcxx=libstdc++11'"
+            )
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["FBXSDK_SDKS"] = os.getenv('FBXSDK_SDKS', 'sdk')
+        cmake.definitions["FBXSDK_SDKS"] = os.getenv("FBXSDK_SDKS", "sdk")
         cmake.configure()
         cmake.build()
