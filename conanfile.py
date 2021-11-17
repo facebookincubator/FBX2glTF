0541a352cfd4d70af1bcf933ebc532cae838c0c1
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-17 09:13:40 -0800

    Add 1.15.

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class FBX2glTFConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = (
         ("boost/1.76.0"),
-        ("libiconv/1.16"),
+        ("libiconv/1.15"),
         ("zlib/1.2.11"),
         ("libxml2/2.9.12"),
         ("fmt/5.3.0"),
